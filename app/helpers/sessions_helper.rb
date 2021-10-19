f42d447f7f61a960b96455edbf630fb713a2cfbf
huongctt-1149 <chu.thi.thanh.huong@sun-asterisk.com>
2021-10-19 15:52:35 +0700

    edit login basic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,7 @@
 module SessionsHelper
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    return if session[:user_id].blank?
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
